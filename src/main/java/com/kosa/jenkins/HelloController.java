f59703c82d2b418d4b0b1eec19585512403c915f
O-Ojinnie <o.ojinnie@gmail.com>
2023-09-26 11:19:11 +0900

    Commit

---
+++ src/main/java/com/kosa/jenkins/HelloController.java
@@ -0,0 +1,17 @@
+package com.kosa.jenkins;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("message", "Hello Kosa!!!");
+		
+		return "hello";
+	}
+
+}
